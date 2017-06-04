ca9a55a473dce9cd103ef359dd2d9fff32d1614e
Gabriel Rodrigues <rodriguez.gabriel001@gmai.com>
2017-06-04 18:47:57 -0300

    Padrão state implementado.

--- src/java/model/state/EstadoFerias.java
+++ src/java/model/state/EstadoFerias.java
@@ -1,10 +1,14 @@
 package model.state;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import model.Estado;
 import model.Funcionarios;
 import model.dao.EstadoJpaController;
+import model.dao.FuncionariosJpaController;
+import model.dao.exceptions.NonexistentEntityException;
 
 @Entity
 @DiscriminatorValue(value = "Férias")
@@ -14,6 +18,13 @@ public class EstadoFerias extends Estado {
     public String ativo(Funcionarios funcionario) {
         Estado ativo = EstadoJpaController.getInstance().findByEstado("Férias");
         funcionario.setEstadoId(ativo);
+        try {
+            FuncionariosJpaController.getInstance().edit(funcionario);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(EstadoAtivo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(EstadoAtivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return "Agora o funcionário está " + ativo.getEstado();
     }
 
